d1587e5867ae14da212c8da859641ba10622165b
mfursov <mike.fursov@gmail.com>
2015-09-20 18:27:41 -0700

    Finishing initial scheduler impl

--- src/main/java/com/github/cs4j/Scheduler.java
+++ src/main/java/com/github/cs4j/Scheduler.java
@@ -1,36 +1,48 @@
 package com.github.cs4j;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.Closeable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 /**
- * Scheduler granularity is 5 seconds.
+ * Cron scheduler implementation. Use Scheduler::schedule method to enable scheduling for all methods
+ * annotated with Scheduled annotation.
  */
 public class Scheduler implements Closeable {
 
+    @NotNull
     private final List<Task> tasks = new ArrayList<>();
-    private final List<Task> runningTasks = new ArrayList<>();
-    private final ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(1);
 
-    public Scheduler() {
-        scheduler.scheduleAtFixedRate((Runnable) new Runnable() {
-            @Override
-            public void run() {
-                Scheduler.this.doSchedule();
-            }
-        }, 10, 5, TimeUnit.SECONDS);
+    @NotNull
+    private final ScheduledExecutorService manager = new ScheduledThreadPoolExecutor(1);
+
+    @NotNull
+    private final ScheduledExecutorService workers;
+
+    public Scheduler(int nThreads) {
+        this(Executors.newScheduledThreadPool(nThreads), 5, 5, TimeUnit.SECONDS);
     }
 
-    public void schedule(Object obj) {
+    public Scheduler(@NotNull ScheduledExecutorService s, int initialDelay, int checkInterval, TimeUnit timeUnit) {
+        this.manager.scheduleAtFixedRate((Runnable) new Runnable() {
+            @Override
+            public void run() {
+                doSchedule();
+            }
+        }, initialDelay, checkInterval, timeUnit);
+        this.workers = s;
+    }
+
+
+    public void schedule(@NotNull Object obj) {
         for (Method m : obj.getClass().getMethods()) {
             Scheduled annotation = m.getAnnotation(Scheduled.class);
             if (annotation != null) {
@@ -41,31 +53,31 @@ public class Scheduler implements Closeable {
         }
     }
 
-
     private void doSchedule() {
         synchronized (tasks) {
             long currentMillis = System.currentTimeMillis();
             for (Task t : tasks) {
-                if (t.executorThread != null) {
+                if (t.nextSchedulingTime >= currentMillis) {
                     continue;
                 }
-                if (t.nextExecutionTime < currentMillis) {
-                    t.start();
+                if (t.scheduled) {
+                    t.nextSchedulingTime = t.sequenceGenerator.next(currentMillis);
+                    continue;
+                }
+                try {
+                    t.lastSchedulingTime = System.currentTimeMillis();
+                    workers.schedule(t, 0, TimeUnit.NANOSECONDS);
+                    t.scheduled = true;
+                } catch (RejectedExecutionException e) {
+                    //todo:
                 }
             }
         }
     }
 
     public void close() {
-        synchronized (runningTasks) {
-            runningTasks.forEach(new Consumer<Task>() {
-                @Override
-                public void accept(Task task) {
-                    task.shutdown();
-                }
-            });
-        }
-        scheduler.shutdown();
+        manager.shutdown();
+        workers.shutdown();
     }
 
     private class Task implements Runnable {
@@ -76,11 +88,9 @@ public class Scheduler implements Closeable {
         @NotNull
         final CronSequenceGenerator sequenceGenerator;
 
-        long lastExecutionTime = 0;
-        long nextExecutionTime = 0;
-
-        @Nullable
-        volatile Thread executorThread;
+        volatile long lastSchedulingTime = 0;
+        volatile long nextSchedulingTime = 0;
+        volatile boolean scheduled;
 
         public Task(@NotNull Object instance, @NotNull Method method, @NotNull CronSequenceGenerator sequenceGenerator) {
             this.instance = instance;
@@ -88,24 +98,6 @@ public class Scheduler implements Closeable {
             this.sequenceGenerator = sequenceGenerator;
         }
 
-        public synchronized void start() {
-            if (executorThread != null) {
-                throw new IllegalStateException("Already running : " + this);
-            }
-            Thread t = new Thread(this);
-            executorThread = t;
-            lastExecutionTime = System.currentTimeMillis();
-            runningTasks.add(this);
-            t.start();
-        }
-
-        public synchronized void shutdown() {
-            Thread t = executorThread;
-            if (t != null) {
-                executorThread = null;
-                t.interrupt();
-            }
-        }
 
         public void run() {
             try {
@@ -113,10 +105,8 @@ public class Scheduler implements Closeable {
             } catch (Exception e) {
                 //TODO: log.error("", e);
             } finally {
-                synchronized (this) {
-                    nextExecutionTime = sequenceGenerator.next(System.currentTimeMillis());
-                    executorThread = null;
-                }
+                nextSchedulingTime = sequenceGenerator.next(System.currentTimeMillis());
+                scheduled = false;
             }
         }
     }
